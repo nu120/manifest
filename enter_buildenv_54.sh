58b6e592e6befd90c93d3bb2ac924d3e66582d2a
Inho Oh <inho.oh@sk.com>
2024-01-17 13:33:26 +0900

    fix docker image name and toolchain
    Signed-off-by: Inho Oh <inho.oh@sk.com>

---
+++ enter_buildenv_54.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+IMAGE=ghcr.io/nu120/buildroot:evk54
+WORKDIR=/opt/buildroot
+
+update() {
+	echo -e "\e[7mUpdate the docker image to latest\e[0m"
+	docker pull $IMAGE
+}
+
+start() {
+	echo -e "\e[7mStart docker container without ccache\e[0m"
+	docker run -it --rm \
+		-v $PWD:$WORKDIR \
+		-w $WORKDIR \
+		$IMAGE
+}
+
+start_with_ccache() {
+	echo -e "\e[7mStart docker container with ccache\e[0m"
+	docker run -it --rm \
+		-v $PWD:$WORKDIR \
+		-v $HOME/cache/dl:$WORKDIR/buildroot/dl \
+		-v $HOME/cache/ccache:/root/.buildroot-ccache  \
+		-w $WORKDIR \
+		$IMAGE
+}
+
+update
+if [ -d $HOME/cache ]; then
+	start_with_ccache
+else
+	start
+fi
+
